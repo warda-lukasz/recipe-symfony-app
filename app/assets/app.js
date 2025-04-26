679ab023b1e7d2bb7d9d08748de18b5e3038eb04
Łukasz Warda <lukasz.warda@mokyokit.com>
2025-04-26 15:41:23 +0200

    feat(ui): replace emoji with FontAwesome icons
    - Zamieniono emotikony na ikony FontAwesome dla lepszej spójności
    - Zmieniono logikę ToggleFavourites na przekierowanie zamiast odpowiedzi
    JSON
    - Dostosowano stylizację ikon (dodano fioletowy kolor dla elementów <i>)
    - Poprawiono układ i formatowanie elementów menu nawigacyjnego

--- app/assets/app.js
+++ app/assets/app.js
@@ -1,5 +1,5 @@
-import './styles/app.css';
 // import './favouriteHandler.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './vendor/bootstrap/bootstrap.index.js';
 import './vendor/fontawesome/css/all.min.css';
+import './styles/app.css';
--- app/src/Controller/Recipe/ToggleFavourites.php
+++ app/src/Controller/Recipe/ToggleFavourites.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller\Recipe;
 
+use App\Controller\AddFlashTrait;
 use App\Controller\BaseController;
 use App\Dto\FavouriteDTO;
 use App\Entity\Recipe;
@@ -11,24 +12,32 @@ use App\Messenger\CommandBus\Command\ToggleFavourites as CommandToggleFavourites
 use App\Messenger\CommandBus\CommandBusInterface;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\RouterInterface;
 use Twig\Environment;
 
 #[AsController]
 #[Route('recipe/{id}/toggle-favourites', name: 'recipe_toggle_favourites')]
 class ToggleFavourites extends BaseController
 {
+
+    use AddFlashTrait;
+
     public function __construct(
         protected Environment $twig,
         private Security $security,
         private CommandBusInterface $commandBus,
+        private RequestStack $requestStack,
+        private RouterInterface $router
     ) {
         parent::__construct($twig);
     }
 
-    public function __invoke(Recipe $recipe): JsonResponse
+    public function __invoke(Recipe $recipe): Response
     {
         $username = $this->security->getUser()->getUsername();
         $this->commandBus->dispatch(
@@ -40,13 +49,12 @@ class ToggleFavourites extends BaseController
             )
         );
 
-        return new JsonResponse(
-            [
-                'status' => 'success',
-                'message' => 'Recipe toggled as favourite',
-                'isFav' => $recipe->isFavouredByUser($username),
-            ],
-            Response::HTTP_OK
-        );
+        if ($recipe->isFavouredByUser($username)) {
+            $this->addFlash('success', "Recipe {$recipe->getTitle()} removed from favourites");
+        } else {
+            $this->addFlash('success', "Recipe {$recipe->getTitle()} added to favourites");
+        }
+
+        return new RedirectResponse($this->router->generate('recipe_list'));
     }
 }
